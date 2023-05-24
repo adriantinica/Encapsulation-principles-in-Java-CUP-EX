896e321daf334a9b455d5e04c9b9f846cfdb949c
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2023-05-24 20:31:07 +0300

    Update Application.java

--- Application.java
+++ Application.java
@@ -1,6 +1,7 @@
 package main;
+import kitchen.BigCup;
 import kitchen.Cup;
-import kitchen.smallCup;
+import kitchen.SmallCup;
 public class Application{
     public static void main(String[] args) {
         
@@ -17,11 +18,15 @@ public class Application{
 
         //creați 1 cana mică de ceai (200 ml) .....afișați informații despre aceasta!
 
-        smallCup smallCup = new smallCup("Tea",(short)200);
+        SmallCup smallCup = new SmallCup("Tea",(short)200);
 
+        // 2. creați o cană mare în main() unde există deja 200 ml de apă
 
-        System.out.println(cupA.getLiquidVolume());
-        System.out.println(cupB.getLiquidVolume());
+        BigCup bigCup = new BigCup("Water",(short)200);
+        
+
+       // System.out.println(cupA.getLiquidVolume());
+       // System.out.println("cupB statement: "+cupB.getLiquidVolume());
 
         //varsati 50 ml de lapte din cana "cupA"
 
@@ -35,15 +40,32 @@ public class Application{
         //afisati in consola informatia despre "cupA" si "cupB"
 
         System.out.println(cupA.getLiquidVolume());
-        System.out.println(cupB.getLiquidVolume());
+        System.out.println("cupB statement: "+cupB.getLiquidVolume());
 
        // ETAPA 2 
        // .....afișați informații despre aceasta!
 
-        System.out.println(smallCup.getLiquidVolume());
-        smallCup.setLiquidVolume((short)200);
-        System.out.println(smallCup.getLiquidVolume());
+        System.out.println(smallCup.getLiquidVolume());  // smallCup este gol
+        smallCup.setLiquidVolume((short)200);  // l-am umplut cu 200 ml lichid
+        System.out.println("smallCup volume: "+ smallCup.getLiquidVolume()); //l-am afisat pe ecran
+
+
+        
+      
+        System.out.println("bigCup volume: "+ bigCup.getLiquidVolume());
+
+        //turnați toată apa rămasă din cupB (part1) 
+        //în această cană mare, afișați informațiile pe ecran!
+        
+
+        
+        bigCup.setLiquidVolume((short)(cupB.getLiquidVolume()+bigCup.getLiquidVolume())); // umplerea celui bigCup
+        cupB.setLiquidVolume((short)0);  // golirea paharului cupB
+        System.out.println("cupB after pouring out: "+ cupB.getLiquidVolume());
+        System.out.println("bigCup after pouring in: "+ bigCup.getLiquidVolume());
+
     }   
+
 
 
 }
