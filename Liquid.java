0b94cad53290516b7a3c27991fb724d61d7010c7
Tinica Adrian <118120924+adriantinica@users.noreply.github.com>
2023-05-25 12:15:28 +0300

    A new class Liquid
    It displays the liquid objects and can tell if that is cold , warm or hot.

---
+++ Liquid.java
@@ -0,0 +1,103 @@
+/*
+ * https://github.com/dorinesinenco/EDUQATION/blob/master/programming/java/oop/encapsulation/kitchen-cup.PART3.DYN.ro.md
+ */
+package world;
+
+public class Liquid {
+    
+    private String liquidName ;
+    private short liquidVolume;
+    private byte liquidTemp;
+
+
+    //  4.Adaugati 2 variante pentru constructor (default si parametrizat) - 
+    //    Cum se numeste acest lucru - atunci cand o metoda are mai multe variante? --- Polymorphism
+
+    public Liquid(){
+
+    }
+
+
+    public Liquid(String liquidName, short liquidVolume, byte liquidTemp){
+        this.setLiquidName(liquidName); 
+        this.setLiquidVolume(liquidVolume); 
+        this.setLiquidTemp(liquidTemp);
+    }
+
+    public String getLiquidName() {
+        return liquidName;
+    }
+
+
+    public void setLiquidName(String liquidName) {
+        if(liquidName == null){
+            System.err.println("Error: Liquid name is not inserted !!!");
+        }else{
+            this.liquidName = liquidName;
+        }
+       
+    }
+
+
+    public short getLiquidVolume() {
+        return liquidVolume;
+    }
+
+
+    public void setLiquidVolume(short liquidVolume) {
+        if (liquidVolume <= 0) {
+            System.err.println("Error: The volume cant be null or negative !!!");
+        } else {
+            this.liquidVolume = liquidVolume;
+        }
+    }   
+
+
+    public byte getLiquidTemp() {
+        return liquidTemp;
+    }
+
+
+    public void setLiquidTemp(byte liquidTemp) {
+        if (liquidTemp >=0 && liquidTemp <= 100) {
+            this.liquidTemp = liquidTemp;
+        } else {
+            System.err.println("Error: State of aggregation must be liquid");
+        }
+    }
+        
+    public boolean isCold(){
+        if (liquidTemp <=18 ) {
+            System.out.println("The liquid is Cold");
+            return true;
+        } else {
+            System.out.println("The liquid is Warm or Hot");
+            return false; 
+        }
+        
+    }
+
+    public boolean isWarm(){
+        if(liquidTemp> 18 && liquidTemp <=36) {
+            System.out.println("The liquid is Warm");
+            return true;
+        } else {
+            System.out.println("The liquid is Hot or Cold");
+            return false; 
+        }
+        
+    }
+
+    public boolean isHot(){
+        if (liquidTemp > 36) {
+            System.out.println("WARNING: HOT LIQUID");
+            return true;
+        } else {
+            System.out.println("This liquid is Cold or Warm");
+            return false;
+        }
+    }
+    
+
+
+}
